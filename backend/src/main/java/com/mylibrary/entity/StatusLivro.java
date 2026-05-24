368a74f5883b84a5ddc1fba48fc143f37b1de1a5
Pedro Lucas <facul.pl05@gmail.com>
2026-05-24 19:38:51 -0300

    feat(backend): adicionar entidade Livro e StatusLivro (#2)

---
+++ backend/src/main/java/com/mylibrary/entity/StatusLivro.java
@@ -0,0 +1,6 @@
+package com.mylibrary.entity;
+
+public enum StatusLivro {
+    DISPONIVEL,
+    EMPRESTADO
+}
---
+++ backend/src/main/java/com/mylibrary/repository/LivroRepository.java
@@ -0,0 +1,21 @@
+package com.mylibrary.repository;
+
+import com.mylibrary.entity.Livro;
+import com.mylibrary.entity.StatusLivro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    List<Livro> findByCategoriaId(Long categoriaId);
+
+    List<Livro> findByStatus(StatusLivro status);
+
+    List<Livro> findByTituloContainingIgnoreCaseOrAutorContainingIgnoreCase(
+            String titulo,
+            String autor
+    );
+
+    boolean existsByCategoriaId(Long categoriaId);
+}
